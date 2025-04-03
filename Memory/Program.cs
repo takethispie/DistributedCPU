c84030edb8b4589f0695dc40657095b00d1ba7ea
Alexandre Felix <takethispie@gmail.com>
2025-04-03 21:47:46 +0200

    fix docker config

--- Memory/Program.cs
+++ Memory/Program.cs
@@ -1,9 +1,6 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using MassTransit;
 
